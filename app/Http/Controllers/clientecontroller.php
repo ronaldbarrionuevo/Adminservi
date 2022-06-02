39f92c7b479bffee23275594f6f615a0870aa4d7
ronald barrionuevo <barrionuevoronald488@gmail.com>
2022-06-02 17:34:26 -0400

    first commit

---
+++ app/Http/Controllers/clientecontroller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class clientecontroller extends Controller
+{
+    function index (){
+
+    }
+    function edit (){
+
+    }
+    function update (){
+
+    }
+    function new (){
+
+    }
+}
